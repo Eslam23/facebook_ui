0f432d174bd3053c70386dcc0699196c1b60ea19
Eslam23 <eslammsaid2391998@gmail.com>
2021-09-16 03:04:13 +0200

    facebook page ui

---
+++ lib/data/data.dart
@@ -0,0 +1,228 @@
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+final User currentUser = User(
+  name: 'Eslam Said',
+  imageUrl:
+      'https://cnn-arabic-images.cnn.io/cloudinary/image/upload/w_1920,c_scale,q_auto/cnnarabic/2021/05/12/images/184749.webp',
+);
+
+final List<User> onlineUsers = [
+  User(
+    name: 'Ahmed said',
+    imageUrl:
+        'https://s.france24.com/media/display/c254fa04-074f-11ec-b901-005056bfb2b6/w:980/p:16x9/AP090415022483.webp',
+  ),
+  User(
+    name: 'Ahmed Zidan',
+    imageUrl:
+        'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec595d45f39760007b05c07%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D989%26cropX2%3D2480%26cropY1%3D74%26cropY2%3D1564',
+  ),
+  User(
+    name: 'Ahmed El Sayed',
+    imageUrl:
+        'https://scontent.fcai19-4.fna.fbcdn.net/v/t1.6435-9/129385161_215128190025119_2732424484618147169_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFQFi2soNu6UGFlXX1yZyoTIS-I0OJLLZohL4jQ4kstmhwtIo1LsYGvUT5PdhAWodURjcE4MSk3viglo-NpfDYe&_nc_ohc=OfzuWKYMgWoAX-V84tg&_nc_ht=scontent.fcai19-4.fna&oh=1a2d30b37cba3badf5bad48f922d696b&oe=616A0DB7',
+  ),
+  User(
+    name: 'Aya Fares',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Mahmoud mahassib',
+    imageUrl:
+        'https://static.highsnobiety.com/thumbor/PfjPSL8WxyZb3xTDzp-D_ptOXBk=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/01/27110709/neymar-pays-tribute-kobe-bryant-01.jpg',
+  ),
+  User(
+    name: 'Adel Mondy',
+    imageUrl:
+        'https://s.france24.com/media/display/c254fa04-074f-11ec-b901-005056bfb2b6/w:980/p:16x9/AP090415022483.webp',
+  ),
+  User(
+    name: 'Yousef Samy',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Ahmed El Sayed',
+      imageUrl:
+          'https://s.france24.com/media/display/c254fa04-074f-11ec-b901-005056bfb2b6/w:980/p:16x9/AP090415022483.webp'),
+  User(
+    name: 'Ayman Said',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Mohamed Mondy',
+    imageUrl:
+        'https://scontent.fcai19-4.fna.fbcdn.net/v/t1.6435-9/129385161_215128190025119_2732424484618147169_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFQFi2soNu6UGFlXX1yZyoTIS-I0OJLLZohL4jQ4kstmhwtIo1LsYGvUT5PdhAWodURjcE4MSk3viglo-NpfDYe&_nc_ohc=OfzuWKYMgWoAX-V84tg&_nc_ht=scontent.fcai19-4.fna&oh=1a2d30b37cba3badf5bad48f922d696b&oe=616A0DB7',
+  ),
+  User(
+    name: 'Gamal Mohamed',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: ' Mohamed Khaled ',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Ezz Asaad',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'Tarek Ali',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Ebrahim Ragab',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'Wael Ragab',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Mohamed Samy',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Yehia Khaled',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=675&q=80'),
+  User(
+    name: 'Ahmed Mido',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Samy Taha',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+];
+
+final List<Story> stories = [
+  Story(
+    user: onlineUsers[0],
+    imageUrl:
+        'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec595d45f39760007b05c07%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D989%26cropX2%3D2480%26cropY1%3D74%26cropY2%3D1564',
+  ),
+  Story(
+    user: onlineUsers[1],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[4],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+  Story(
+    user: onlineUsers[5],
+    imageUrl:
+        'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[8],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+];
+
+final List<Post> posts = [
+  Post(
+    user: currentUser,
+    caption: 'Mohamed Salah goal on totenham team on premier league ',
+    timeAgo: '58m',
+    imageUrl:
+        'http://images.performgroup.com/di/library/omnisport/92/3e/mohamedsalah-cropped_1dkncgxnpaxrp1pptzxk3wyh0m.jpg?t=339858707&t=339858707&w=1200&h=630',
+    likes: 1202,
+    comments: 184,
+    shares: 96,
+  ),
+  Post(
+    user: onlineUsers[5],
+    caption: 'Watch: Cristiano Ronaldo scores twice as Juventus beat Cagliari.',
+    timeAgo: '3hr',
+    imageUrl: 'https://images.indianexpress.com/2020/11/Cristiano-Ronaldo.jpg',
+    likes: 683,
+    comments: 79,
+    shares: 18,
+  ),
+  Post(
+    user: onlineUsers[4],
+    caption: 'Hala Madrid .',
+    timeAgo: '8hr',
+    imageUrl:
+        'https://www.greentidekw.com/wp-content/uploads/2021/03/%D8%B1%D9%8A%D8%A7%D9%84-%D9%85%D8%AF%D8%B1%D9%8A%D8%AF.jpg',
+    likes: 894,
+    comments: 201,
+    shares: 27,
+  ),
+  Post(
+    user: onlineUsers[3],
+    caption:
+        'When you have a champion like Ronaldo you can achieve any result, he is an example for everyone and he only has to continue like this,” Juventus coach Andrea Pirlo said.',
+    timeAgo: '15hr',
+    imageUrl: null,
+    likes: 722,
+    comments: 183,
+    shares: 42,
+  ),
+  Post(
+    user: onlineUsers[0],
+    caption:
+        'Do not be sad for your kindness and your good opinion of others, if there is no one on earth who appreciates it, then in heaven there is someone who blesses it.',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+  Post(
+    user: onlineUsers[9],
+    caption: 'A classic.',
+    timeAgo: '1d',
+    imageUrl:
+        'https://images.unsplash.com/reserve/OlxPGKgRUaX0E1hg3b3X_Dumbo.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    likes: 1523,
+    shares: 129,
+    comments: 301,
+  )
+];
